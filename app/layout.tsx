3eb49409d22317e2ce7f366385ae6bcb4adc3c05
Tenma <tenma@osxt.com>
2024-02-23 12:09:58 +0100

    change favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/pokemon.png" sizes="any" />
+      </head>
       <body
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
